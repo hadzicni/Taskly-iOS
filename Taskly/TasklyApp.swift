9de8d5edad765c6c27b7118ff216b68e68cfde11
Nikola Hadzic <bibilionyt@gmail.com>
2025-05-02 17:50:37 +0200

    Initial Commit

---
+++ Taskly/TasklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasklyApp.swift
+//  Taskly
+//
+//  Created by Nikola Hadzic on 02.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TasklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
